8ea597b3e18a9b46fc0e17b4391c0dc509ca38c4
Phy Llova <jenshllova@acelinkcourier.com>
2023-08-31 16:06:47 -0700

    added files

--- js/index.js
+++ js/index.js
@@ -19,19 +19,14 @@ function can() {
   document.getElementById("choose").style.display = "none";
 
 }
-function facebook() {
+function fb() {
   document.getElementById("fbp").style.display = "block";
   document.getElementById("home").style.display = "none";
   document.getElementById("choose").style.display = "none";
 
 }
-function instagram() {
+function ig() {
   document.getElementById("igp").style.display = "block";
-  document.getElementById("home").style.display = "none";
-  document.getElementById("choose").style.display = "none";
-}
-function twitter() {
-  document.getElementById("twp").style.display = "block";
   document.getElementById("home").style.display = "none";
   document.getElementById("choose").style.display = "none";
 }
